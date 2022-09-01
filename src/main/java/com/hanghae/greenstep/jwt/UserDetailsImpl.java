08c900e94e5098924a47394ee41f2fae89dffe62
DESKTOP-KKQ64SH\LG PC <hajh5447@naver.com>
2022-09-01 14:46:14 +0900

    fix: 로그인 시 이메일로 userDetail 확인하도록 수정

--- src/main/java/com/hanghae/greenstep/jwt/UserDetailsImpl.java
+++ src/main/java/com/hanghae/greenstep/jwt/UserDetailsImpl.java
@@ -37,7 +37,7 @@ public class UserDetailsImpl implements UserDetails {
 
     @Override
     public String getUsername(){
-        return member.getNickname();
+        return member.getEmail();
     }
 
     @Override
