b1f7b4bf9a2c8ed69bc33416c05fe72453dbe87d
kyuhyun-sim <zaq7974@naver.com>
2022-09-03 15:36:29 +0900

    fix: feed 전체 조회 무한스크롤 기능 추가

--- src/main/java/com/hanghae/greenstep/feed/FeedRepository.java
+++ src/main/java/com/hanghae/greenstep/feed/FeedRepository.java
@@ -1,10 +1,12 @@
 package com.hanghae.greenstep.feed;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface FeedRepository extends JpaRepository<Feed,Long> {
-    List<Feed> findAllByOrderByCreatedAtDesc();
+    List<Feed> findByIdLessThanInOrderByIdDesc(int lastFeedId, PageRequest pageRequest);
 }
 
