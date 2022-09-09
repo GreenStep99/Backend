d4bcbc6b081f4935925643bc6bed3944f2324294
kyuhyun-sim <zaq7974@naver.com>
2022-09-09 11:39:45 +0900

    fix: AdminService 의 n:1 연관관계 매핑 n+1 문제 fetch join으로 해결

--- src/main/java/com/hanghae/greenstep/admin/ApplicationEventPublisher.java
+++ src/main/java/com/hanghae/greenstep/admin/ApplicationEventPublisher.java
@@ -2,8 +2,6 @@ package com.hanghae.greenstep.admin;
 
 import org.springframework.stereotype.Component;
 
-import static com.hanghae.greenstep.shared.Status.DONE;
-
 @Component
 public class ApplicationEventPublisher {
 
--- src/main/java/com/hanghae/greenstep/submitMission/SubmitMissionRepository.java
+++ src/main/java/com/hanghae/greenstep/submitMission/SubmitMissionRepository.java
@@ -7,9 +7,15 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface SubmitMissionRepository extends JpaRepository<SubmitMission,Long> {
-    List<SubmitMission> findAllByOrderByCreatedAtAsc();
+
+    @Query("select s from SubmitMission s join fetch s.member order by s.createdAt asc")
+    List<SubmitMission> findAllByOrderByCreatedAtAscFetchJoin();
+
+    @Query("select s from SubmitMission s join fetch s.member where s.member.id = :submitMissionId")
+    Optional<SubmitMission> findByIdFetchJoin(Long submitMissionId);
 
     @Query("select s from SubmitMission s where s.status = 'DONE' AND s.member = :member order by s.createdAt desc ")
     List<SubmitMission> findAllByMember(@Param("member") Member member);
