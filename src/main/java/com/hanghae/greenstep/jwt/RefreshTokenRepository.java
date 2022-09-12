dae681cc99074f66d94e70764370776daae747f6
DESKTOP-KKQ64SH\LG PC <hajh5447@naver.com>
2022-09-12 13:39:01 +0900

    feat: 로그아웃, 회원탈퇴 처리 구현

--- src/main/java/com/hanghae/greenstep/jwt/RefreshTokenRepository.java
+++ src/main/java/com/hanghae/greenstep/jwt/RefreshTokenRepository.java
@@ -4,10 +4,12 @@ package com.hanghae.greenstep.jwt;
 import com.hanghae.greenstep.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.swing.plaf.metal.MetalMenuBarUI;
 import java.util.Optional;
 
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken,Long> {
 
     Optional<RefreshToken> findByMember(Member member);
 
+    void deleteByMember(Member member);
 }
