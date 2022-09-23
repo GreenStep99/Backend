0dc1f875ea1c5cce8f03b2d920173bc4f70ea0a3
kyuhyun-sim <108973592+kyuhyun-sim@users.noreply.github.com>
2022-09-23 17:17:36 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -18,16 +18,5 @@ else
   sleep 5
 fi
 
-echo "> 새 애플리케이션 배포"
-
-JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
-
-echo "> JAR NAME: $JAR_NAME"
-
-echo "> $JAR_NAME 에 실행권한 추가"
-
-chmod +x $JAR_NAME
-
-echo "> $JAR_NAME 실행"
-
-nohup java -jar $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+echo "> $JAR_PATH 배포"
+nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
