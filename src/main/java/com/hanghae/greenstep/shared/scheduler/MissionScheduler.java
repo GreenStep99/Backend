beba0d3ad938f1ac085ce3e1e05b7f19eb7f2dba
kyuhyun-sim <zaq7974@naver.com>
2022-09-28 17:57:15 +0900

    주석처리

--- src/main/java/com/hanghae/greenstep/shared/scheduler/MissionScheduler.java
+++ src/main/java/com/hanghae/greenstep/shared/scheduler/MissionScheduler.java
@@ -35,47 +35,47 @@ public class MissionScheduler {
             member.resetDailyPoint();
         }
         missionStatusRepository.deleteAllDailyMissionStatus();
-        List<Mission> allDailyMissionList = missionRepository.findAllDailyMission();
-        for (Mission mission : allDailyMissionList) {
-            mission.updateOnShow(false);
-        }
-        List<Mission> dailyMissionList = missionRepository.findDailyMission();
-        for (Mission mission : dailyMissionList) {
-            log.info(mission.getMissionName());
-            mission.updateOnShow(true);
-        }
+//        List<Mission> allDailyMissionList = missionRepository.findAllDailyMission();
+//        for (Mission mission : allDailyMissionList) {
+//            mission.updateOnShow(false);
+//        }
+//        List<Mission> dailyMissionList = missionRepository.findDailyMission();
+//        for (Mission mission : dailyMissionList) {
+//            log.info(mission.getMissionName());
+//            mission.updateOnShow(true);
+//        }
     }
 
-    @Scheduled(cron = "0 0 0 ? * SUN")
-    @Transactional
-    public void resetAndUpdateWeeklyMission() {
-        log.info("Scheduled Run");
-        missionStatusRepository.deleteAllWeeklyMissionStatus();
-        List<Mission> allWeeklyMissionList = missionRepository.findAllWeeklyMission();
-        for (Mission mission : allWeeklyMissionList) {
-            mission.updateOnShow(false);
-        }
-        List<Mission> weeklyMissionList = missionRepository.findWeeklyMission();
-        for (Mission mission : weeklyMissionList) {
-            log.info(mission.getMissionName());
-            mission.updateOnShow(true);
-        }
-    }
-
-    @Scheduled(cron = "0 0 0 1/1 * ?")
-//    @Scheduled(cron = "0 0/1 * 1/1 * ?")
-    @Transactional
-    public void resetAndUpdateChallengeMission() {
-        log.info("Scheduled Run");
-        missionStatusRepository.deleteAllChallengeMissionStatus();
-        List<Mission> allChallengeMissionList = missionRepository.findAllChallengeMission();
-        for (Mission mission : allChallengeMissionList) {
-            mission.updateOnShow(false);
-        }
-        List<Mission> challengeMissionList = missionRepository.findChallengeMission();
-        for (Mission mission : challengeMissionList) {
-            log.info(mission.getMissionName());
-            mission.updateOnShow(true);
-        }
-    }
+//    @Scheduled(cron = "0 0 0 ? * SUN")
+//    @Transactional
+//    public void resetAndUpdateWeeklyMission() {
+//        log.info("Scheduled Run");
+//        missionStatusRepository.deleteAllWeeklyMissionStatus();
+//        List<Mission> allWeeklyMissionList = missionRepository.findAllWeeklyMission();
+//        for (Mission mission : allWeeklyMissionList) {
+//            mission.updateOnShow(false);
+//        }
+//        List<Mission> weeklyMissionList = missionRepository.findWeeklyMission();
+//        for (Mission mission : weeklyMissionList) {
+//            log.info(mission.getMissionName());
+//            mission.updateOnShow(true);
+//        }
+//    }
+//
+//    @Scheduled(cron = "0 0 0 1/1 * ?")
+////    @Scheduled(cron = "0 0/1 * 1/1 * ?")
+//    @Transactional
+//    public void resetAndUpdateChallengeMission() {
+//        log.info("Scheduled Run");
+//        missionStatusRepository.deleteAllChallengeMissionStatus();
+//        List<Mission> allChallengeMissionList = missionRepository.findAllChallengeMission();
+//        for (Mission mission : allChallengeMissionList) {
+//            mission.updateOnShow(false);
+//        }
+//        List<Mission> challengeMissionList = missionRepository.findChallengeMission();
+//        for (Mission mission : challengeMissionList) {
+//            log.info(mission.getMissionName());
+//            mission.updateOnShow(true);
+//        }
+//    }
 }
